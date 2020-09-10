a452d7bd72b76440f8dcefa6668f6d876682042b
Troy Su <sujinrong@hotoa.com>
2020-09-10 19:07:17 +0800

    放开所有对象，新增自定义按钮，调整对象之间的子表关系

---
+++ src/crm/opportunity_product.action.js
@@ -0,0 +1,9 @@
+module.exports = {
+  selectPriceBook: function(){
+      toastr.success("选择价格手册，待开发");
+    },
+  
+    selectPriceBookVisible: function(){
+      return true
+    }
+  }
---
+++ src/objects/account_contact_relationship.action.js
@@ -0,0 +1,9 @@
+module.exports = {
+  addContact: function(){
+        toastr.success("新建联系人，待开发");
+      },
+    
+      addContactVisible: function(){
+        return true
+      }
+    }
