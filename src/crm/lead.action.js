92f2ab9c4c01f4e12686c21fe9f081bd088319c7
Troy Su <sujinrong@hotoa.com>
2020-09-11 18:47:04 +0800

    去除隐藏字段注释

--- src/crm/lead.action.js
+++ src/crm/lead.action.js
@@ -1,6 +1,6 @@
 module.exports = {
     convert: function(){
-      toastr.success("潜在客户可转换成商机，即将上线，敬请期待");
+      toastr.success("潜在客户可转换成业务机会，即将上线，敬请期待");
     },
   
     convertVisible: function(){
