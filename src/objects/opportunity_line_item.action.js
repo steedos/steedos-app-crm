e54d10a764e5aaac55484ad040b172f0969e0c8e
Troy Su <sujinrong@hotoa.com>
2020-09-24 16:33:19 +0800

    添加多个自定义按钮

---
+++ src/objects/opportunity_line_item.action.js
@@ -0,0 +1,14 @@
+module.exports = {
+  selectPriceBook: function(){
+    toastr.success("选择价格手册，即将上线，敬请期待");
+  },
+  selectPriceBookVisible: function(){
+    return true
+  },
+  addProduct: function(){
+    toastr.success("添加产品，即将上线，敬请期待");
+  },
+  addProductVisible: function(){
+    return true
+  }
+  }
---
+++ src/objects/product.action.js
@@ -0,0 +1,8 @@
+module.exports = {
+    addToPriceBook: function(){
+      toastr.success("添加到价格手册，即将上线，敬请期待");
+    },
+    addToPriceBookVisible: function(){
+      return true
+    }
+    }
