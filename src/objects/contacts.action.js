e1a64e1969a74020e92e36e59ed9c9edbef914bf
殷亮辉 <yinlianghui@hotoa.com>
2020-10-14 14:37:27 +0800

    转es5
    https://github.com/steedos/steedos-platform/issues/964

--- src/objects/contacts.action.js
+++ src/objects/contacts.action.js
@@ -23,17 +23,23 @@ module.exports = {
         }
     },
     viewCustomerSpaceUser: function(){
-        var record = Creator.odata.query("space_users", {$filter: `(user eq '${this.record.user}')`}, true);
+        var record = Creator.odata.query("space_users", {
+            $filter: "(user eq '".concat(this.record.user, "')")
+        }, true);
         FlowRouter.go(Creator.getObjectRouterUrl("space_users", record[0]._id));
     },
     viewCustomerSpaceUserVisible: function(object_name, record_id, record_permissions){
-        if(!Creator.isSpaceAdmin()){
-            return false
+        if (!Creator.isSpaceAdmin()) {
+            return false;
         }
         var record = Creator.odata.get("contacts", record_id, "account,user", "account($select=is_customer)");
-        if(record && record.user){
-            var spaceUser = Creator.odata.query("space_users", {$filter: `(user eq '${record.user}')`, $select: "is_customer"}, true);
-            if(spaceUser[0].is_customer){
+        if (record && record.user) {
+            var spaceUser = Creator.odata.query("space_users", {
+                $filter: "(user eq '".concat(record.user, "')"),
+                $select: "is_customer"
+            }, true);
+    
+            if (spaceUser[0].is_customer) {
                 return true;
             }
         }
@@ -101,13 +107,16 @@ module.exports = {
         })
     },
     disableCustomerSpaceUserVisible: function(object_name, record_id, record_permissions){
-        if(!Creator.isSpaceAdmin()){
-            return false
+        if (!Creator.isSpaceAdmin()) {
+            return false;
         }
         var record = Creator.odata.get("contacts", record_id, "account,user", "account($select=is_customer)");
-        if(record && record.user){
-            var spaceUser = Creator.odata.query("space_users", {$filter: `(user eq '${record.user}')`, $select: "is_customer"}, true);
-            if(spaceUser[0].is_customer){
+        if (record && record.user) {
+            var spaceUser = Creator.odata.query("space_users", {
+                $filter: "(user eq '".concat(record.user, "')"),
+                $select: "is_customer"
+            }, true);
+            if (spaceUser[0].is_customer) {
                 return true;
             }
         }
@@ -136,8 +145,10 @@ module.exports = {
         }
     },
     viewSupplierSpaceUser: function(){
-        var record = Creator.odata.query("space_users", {$filter: `(user eq '${this.record.user}')`}, true);
-        FlowRouter.go(Creator.getObjectRouterUrl("space_users", record[0]._id));	
+        var record = Creator.odata.query("space_users", {
+            $filter: "(user eq '".concat(this.record.user, "')")
+          }, true);
+        FlowRouter.go(Creator.getObjectRouterUrl("space_users", record[0]._id));
     },
     viewSupplierSpaceUserVisible: function(object_name, record_id, record_permissions){
         if(!Creator.isSpaceAdmin()){
