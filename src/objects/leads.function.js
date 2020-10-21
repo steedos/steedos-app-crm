0318b3b81681716a71b6cda005bff604f4088e98
殷亮辉 <yinlianghui@hotoa.com>
2020-10-21 17:34:11 +0800

    #6 跟sf一样转换后需要给业务机会同时新建联系人角色子表记录，如果转换时选择的是已有业务机会，则可能会把新建的联系人累加了新建联系人角色子表中，但是主要联系人角色只有一个且不会自动变更

--- src/objects/leads.function.js
+++ src/objects/leads.function.js
@@ -3,24 +3,77 @@ const core = require('@steedos/core');
 
 const convertSettings = require('./leads_convert.json');
 
-const getDocConverts = (object_name, record)=>{
+const getDocConverts = (object_name, record) => {
   let result = {};
   const converts = Object.assign({}, convertSettings[object_name]);
-  _.each(converts, (item, key)=>{
+  _.each(converts, (item, key) => {
     result[key] = record[item];
   });
   return result;
 }
 
 // 取出docConverts中oldDoc对应key值为空的字段值集合，即docConverts中值不能覆盖更新oldDoc中已经存在的字段值
-const getDocEmptyConverts = (docConverts, oldDoc)=>{
+const getDocEmptyConverts = (docConverts, oldDoc) => {
   let result = {};
-  _.each(docConverts, (item, key)=>{
-    if((oldDoc[key] === undefined || oldDoc[key] === null || oldDoc[key] === 0) && item !== undefined && item !== null && item !== 0){
+  _.each(docConverts, (item, key) => {
+    if ((oldDoc[key] === undefined || oldDoc[key] === null || oldDoc[key] === 0) && item !== undefined && item !== null && item !== 0) {
       result[key] = item;
     }
   });
   return result;
+}
+
+// 计算是否要新建业务机会联系人角色，以及是否是新建主要角色
+const getNewOpportunityContactRoleOptions = async (isLookupOpportunity, isLookupContact, recordOpportunity, recordContact, objOpportunityContactRole) => {
+  let needAddOpportunityContactRole = false;
+  let isPrimaryRole = false;
+  if (isLookupOpportunity && isLookupContact) {
+    // 业务机会和联系人都不是新建的
+    const roleRecords = await objOpportunityContactRole.find({
+      filters: [["opportunity_id", "=", recordOpportunity._id]]
+    });
+    const repeatRoleRecord = roleRecords.find((item) => {
+      return item.contact_id === recordContact._id;
+    });
+    if (!repeatRoleRecord) {
+      // 只有不存在相同联系人的记录时才需要新建联系人角色
+      needAddOpportunityContactRole = true;
+    }
+    if (needAddOpportunityContactRole) {
+      const primaryRoleRecord = roleRecords.find((item) => {
+        return item.is_primary === true;
+      });
+      if (!primaryRoleRecord) {
+        // 只有不存在is_primary为true的记录时才设置当前角色为主要联系人角色
+        isPrimaryRole = true;
+      }
+    }
+  }
+  else if (isLookupOpportunity) {
+    // 是新建的联系人，但是不是新建的业务机会
+    needAddOpportunityContactRole = true;
+    const primaryCount = await objOpportunityContactRole.count({
+      filters: [["opportunity_id", "=", recordOpportunity._id], ["is_primary", "=", true]]
+    });
+    if (primaryCount === 0) {
+      // 只有不存在is_primary为true的记录时才设置当前角色为主要联系人角色
+      isPrimaryRole = true;
+    }
+  }
+  else if (isLookupContact) {
+    // 是新建的业务机会，但是不是新建的联系人
+    needAddOpportunityContactRole = true;
+    isPrimaryRole = true;
+  }
+  else {
+    // 是新建的业务机会和联系人
+    needAddOpportunityContactRole = true;
+    isPrimaryRole = true;
+  }
+  return {
+    needAddOpportunityContactRole,
+    isPrimaryRole
+  }
 }
 
 module.exports = {
@@ -48,37 +101,37 @@ module.exports = {
       let docLeadUpdate = { converted: true, status: "Qualified" };
       const objAccounts = steedosSchema.getObject('accounts');
       const objContacts = steedosSchema.getObject('contacts');
-      const doc = { owner: body.record_owner_id, space: userSession.spaceId };
+      const baseDoc = { owner: body.record_owner_id, space: userSession.spaceId };
       let recordAccount;
-      if(body.is_lookup_account && body.lookup_account){
+      if (body.is_lookup_account && body.lookup_account) {
         recordAccount = await objAccounts.findOne(body.lookup_account);
-        if(!recordAccount){
+        if (!recordAccount) {
           return res.status(500).send({
             "error": "Action Failed -- The account is not found.",
             "success": false
           });
         }
-          // 所有字段属性都是为空才同步更新
+        // 所有字段属性都是为空才同步更新
         const docAccountEmptyConverts = getDocEmptyConverts(docAccountConverts, recordAccount);
-        if(!_.isEmpty(docAccountEmptyConverts)){
+        if (!_.isEmpty(docAccountEmptyConverts)) {
           await objAccounts.updateOne(recordAccount._id, docAccountEmptyConverts, userSession);
         }
       }
-      else{
-        recordAccount = await objAccounts.insert(Object.assign({}, doc, docAccountConverts, { name: new_account_name }), userSession);
-        if(!recordAccount){
+      else {
+        recordAccount = await objAccounts.insert(Object.assign({}, baseDoc, docAccountConverts, { name: new_account_name }), userSession);
+        if (!recordAccount) {
           return res.status(500).send({
             "error": "Action Failed -- Insert account failed.",
             "success": false
           });
         }
       }
-      if(recordAccount){
+      if (recordAccount) {
         docLeadUpdate.converted_account = recordAccount._id;
         let recordContact;
-        if(body.is_lookup_contact && body.lookup_contact){
+        if (body.is_lookup_contact && body.lookup_contact) {
           recordContact = await objContacts.findOne(body.lookup_contact);
-          if(!recordContact){
+          if (!recordContact) {
             return res.status(500).send({
               "error": "Action Failed -- The contact is not found.",
               "success": false
@@ -86,17 +139,17 @@ module.exports = {
           }
           // 包括所属客户在内，所有字段属性都是为空才同步更新
           let docContactEmptyConverts = getDocEmptyConverts(Object.assign({}, docContactConverts, { account: recordAccount._id }), recordContact);
-          if(body.force_update_contact_lead_source && !docContactEmptyConverts.lead_source && docContactConverts.lead_source){
+          if (body.force_update_contact_lead_source && !docContactEmptyConverts.lead_source && docContactConverts.lead_source) {
             // 如果界面上勾选了“更新潜在客户来源”，则应该强行更新联系人的潜在客户来源
             docContactEmptyConverts.lead_source = docContactConverts.lead_source;
           }
-          if(!_.isEmpty(docContactEmptyConverts)){
+          if (!_.isEmpty(docContactEmptyConverts)) {
             await objContacts.updateOne(recordContact._id, docContactEmptyConverts, userSession);
           }
         }
-        else{
-          recordContact = await objContacts.insert(Object.assign({}, doc, docContactConverts, { name: new_contact_name, account: recordAccount._id }), userSession);
-          if(!recordContact){
+        else {
+          recordContact = await objContacts.insert(Object.assign({}, baseDoc, docContactConverts, { name: new_contact_name, account: recordAccount._id }), userSession);
+          if (!recordContact) {
             return res.status(500).send({
               "error": "Action Failed -- Insert contact failed.",
               "success": false
@@ -108,9 +161,9 @@ module.exports = {
           if (!body.omit_new_opportunity) {
             const objOpportunity = steedosSchema.getObject('opportunity');
             let recordOpportunity;
-            if(body.is_lookup_opportunity && body.lookup_opportunity){
+            if (body.is_lookup_opportunity && body.lookup_opportunity) {
               recordOpportunity = await objOpportunity.findOne(body.lookup_opportunity);
-              if(!recordOpportunity){
+              if (!recordOpportunity) {
                 return res.status(500).send({
                   "error": "Action Failed -- The opportunity is not found.",
                   "success": false
@@ -118,13 +171,13 @@ module.exports = {
               }
               // 包括所属客户在内，所有字段属性都是为空才同步更新
               const docOpportunityEmptyConverts = getDocEmptyConverts(Object.assign({}, docOpportunityConverts, { account: recordAccount._id }), recordOpportunity);
-              if(!_.isEmpty(docOpportunityEmptyConverts)){
+              if (!_.isEmpty(docOpportunityEmptyConverts)) {
                 await objOpportunity.updateOne(recordOpportunity._id, docOpportunityEmptyConverts, userSession);
               }
             }
-            else{
-              recordOpportunity = await objOpportunity.insert(Object.assign({}, doc, docOpportunityConverts, { name: new_opportunity_name, account: recordAccount._id }), userSession);
-              if(!recordOpportunity){
+            else {
+              recordOpportunity = await objOpportunity.insert(Object.assign({}, baseDoc, docOpportunityConverts, { name: new_opportunity_name, account: recordAccount._id }), userSession);
+              if (!recordOpportunity) {
                 return res.status(500).send({
                   "error": "Action Failed -- Insert opportunity failed.",
                   "success": false
@@ -133,6 +186,11 @@ module.exports = {
             }
             if (recordOpportunity) {
               docLeadUpdate.converted_opportunity = recordOpportunity._id;
+              const objOpportunityContactRole = steedosSchema.getObject('opportunity_contact_role');
+              const { needAddOpportunityContactRole, isPrimaryRole } = await getNewOpportunityContactRoleOptions(body.is_lookup_opportunity, body.is_lookup_contact, recordOpportunity, recordContact, objOpportunityContactRole);
+              if (needAddOpportunityContactRole) {
+                await objOpportunityContactRole.insert(Object.assign({}, baseDoc, { opportunity_id: recordOpportunity._id, contact_id: recordContact._id, is_primary: isPrimaryRole }), userSession);
+              }
             }
           }
         }
