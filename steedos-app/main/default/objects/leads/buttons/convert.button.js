4a1670df91f33ff39eaa1322f7751991d725e833
Joanna93-JJ <68261251+Joanna93-JJ@users.noreply.github.com>
2020-11-24 18:09:19 +0800

    修改命名

---
+++ steedos-app/main/default/objects/leads/buttons/convert.button.js
@@ -0,0 +1,10 @@
+module.exports = { 
+convert:function () {
+    Steedos.CRM.convertLead(this.record);
+  },
+convertVisible:function (object_name, record_id, permissions, record) {
+    if(record && !record.converted){
+      return true
+    }
+  }
+ }
