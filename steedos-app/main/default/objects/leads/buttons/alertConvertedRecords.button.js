cd9679d11116b9b8da51fe2675819f8007c44266
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-01-14 16:06:53 +0800

    调整潜在客户列表视图

--- steedos-app/main/default/objects/leads/buttons/alertConvertedRecords.button.js
+++ steedos-app/main/default/objects/leads/buttons/alertConvertedRecords.button.js
@@ -1,10 +1,12 @@
-module.exports = { 
-alertConvertedRecords:function () {
-    Steedos.CRM.alertLeadConvertedRecords(this.record);
-  },
-alertConvertedRecordsVisible:function (object_name, record_id, permissions, record) {
-    if(record && record.converted){
-      return true
+module.exports = {
+
+    alertConvertedRecords: function() {
+        Steedos.CRM.alertLeadConvertedRecords(this.record);
+    },
+    alertConvertedRecordsVisible: function(object_name, record_id, permissions, record) {
+        if (record && record.converted) {
+            return true
+        }
     }
-  }
- }
+
+}
